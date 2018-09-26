1e4b03dd9ee57e7085b52a17638fc8c4684c0b56
“SivonneWard” <sivonne@live.unc.edu>
2018-09-26 10:50:05 -0400

    added hamburger menu with fade, need to fix tooltip and overlaying capabilities

---
+++ js/script.js
@@ -0,0 +1,32 @@
+console.log('scripts loaded');
+
+
+var burger = document.getElementById('burger');
+var mobileNav = document.getElementById('offscreen-nav');
+var closeBtn = document.getElementById('closed-button');
+var content = document.getElementById('content');
+var moreInfoText = document.getElementById('more-info');
+burger.addEventListener('click', function(){
+  console.log('button clicked');
+  mobileNav.style.width = '50%';
+  mobileNav.style.transition = '.5s ease';
+  content.style.opacity = '0.1';
+});
+
+closeBtn.addEventListener('click', function(){
+  mobileNav.style.width = '0%';
+  content.style.opacity = '1';
+});
+moreInfoText.addEventListener('mouseenter', function(){
+  tooltip.style.opacity = '1';
+  var x = event.screenX;
+  var y = event.screenY;
+  console.log(x);
+  tooltip.style.top = y = 'px';
+  tooltip.style.right = x = 'px';
+
+});
+moreInfoText.addEventListener('mouseleave', function(){
+  tooltip.style.opacity = '0';
+  cursor.style.shape = 'triangle';
+});
